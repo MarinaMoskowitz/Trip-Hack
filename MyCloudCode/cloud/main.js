d72f68c1302708059a5a3ca401a169347a759f91
Johnny Lau <johnny.df.lau@gmail.com>
2015-03-07 05:13:09 -0500

    Hardcoded tripApi results to app

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -12,16 +12,37 @@ Parse.Cloud.define("hello", function(request, response) {
 
 function getLocationID(lat, long) {
 	// this returns location ID from trip api call
-}
+	// TODO: this is hardcoded test data
+	// 	 that returns Boston locID
+	return 60745
+};
 
 function getPageInfo(locationID) {
-	// this returns a dictionary with attraction,
-	// reviews, ratings, pictures, etc. for app to
+	// this returns a dictionary with photos, 
+	// location, description, rating, num ratings,
+	// title, country photo. etc. for app to
 	// display.
-}
+
+	// TODO: hardcoded. Returns test dictionary
+	info = {
+		'location': 'Boston, Massachusetts',
+		'description': 'walk here for a while, you know, do stuff'',
+		'rating': 5,
+		'number_ratings': 200,
+		'title': 'Freedom Trail',
+		'photos': ['http://e2.tacdn.com/cds/media/photo-f/01/b7/79/d3/freedom-trail-sidewalk.jpg'],
+		'country_photos': ['http://e2.tacdn.com/cds/media/photo-f/01/58/ab/a5/the-view.jpg']
+		}
+	
+	return info;
+};
 
 // request.params.locationData -> {oldData: {lat, long}}
 Parse.Cloud.define("getTripData", function(request, response) {
 	console.log("this is a test:", request.params.locationData);
-}
+	newCoord = request.params.locationData['newCoord']
+	locID = getLocationID(newCoord['lat'], newCoord['long'])
+	info = getPageInfo(locID)
+	response.success(info)
+});
 
