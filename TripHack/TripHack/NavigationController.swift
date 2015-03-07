283a7822df31285a8ae099a9c2c62b9f930fbb01
cwrichardkim93 <cwrichardkim93@gmail.com>
2015-03-06 22:02:23 -0500

    vc setup

---
+++ TripHack/TripHack/NavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  NavigationController.swift
+//  TripHack
+//
+//  Created by Richard Kim on 3/6/15.
+//  Copyright (c) 2015 MarinaLLC. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
